21f2f7d8c2ef3604a1c594b302bf0b4ebbc5e0a1
tomaszrojek <tomasz.rojek.81@gmail.com>
2022-09-12 18:37:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-html-to-pdf-example-answer-1exqgf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-html-to-pdf-example-answer-1exqgf)
---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import {jsPDF} from 'jspdf';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular Html To Pdf ';
+
+  @ViewChild('pdfTable', {static: false}) pdfTable: ElementRef;
+
+
+  public downloadAsPDF() {
+    const doc = new jsPDF();
+
+    const specialElementHandlers = {
+      '#editor': function (element, renderer) {
+        return true;
+      }
+    };
+
+    const pdfTable = this.pdfTable.nativeElement;
+
+    doc.fromHTML(pdfTable.innerHTML, 15, 15, {
+      width: 190,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('tableToPdf.pdf');
+  }
+}
